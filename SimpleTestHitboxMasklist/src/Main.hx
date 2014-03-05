458e1c88f8b144d3b7c8e0f02d4b5650f19122f1
azrafe7 <azrafe7@gmail.com>
2014-03-05 23:01:07 +0100

    Simple test for Hitboxes in Masklist

---
+++ SimpleTestHitboxMasklist/src/Main.hx
@@ -0,0 +1,52 @@
+import com.haxepunk.*;
+import com.haxepunk.graphics.*;
+import com.haxepunk.HXP;
+import com.haxepunk.masks.Circle;
+import com.haxepunk.masks.Hitbox;
+import com.haxepunk.masks.Masklist;
+import com.haxepunk.utils.Input;
+import com.haxepunk.utils.Key;
+import flash.system.System;
+
+
+class TestWorld extends World {
+    public function new():Void {
+        super();
+		
+		var h = new Hitbox(30, 50, 10, 10);
+		
+		var h1 = new Hitbox(30, 50, 10, 10);
+		var h2 = new Hitbox(60, 20, 20, 80);
+		var c = new Circle(30, 0, 0);
+		
+		addMask(new Masklist([h1, h2, c]), "masklist", 50, 50);
+		addMask(h, "hitbox", 50, 200);
+    }
+	
+	override public function update():Void 
+	{
+		if (Input.check(Key.ESCAPE)) quit();
+	}
+	
+	public function quit():Void 
+	{
+	#if (flash || html5)
+		System.exit(1);
+	#else
+		Sys.exit(1);
+	#end
+	}
+}
+
+
+class Main extends Engine {
+    public function new():Void {
+        super(800, 600, 60, false);
+		HXP.console.enable();
+		HXP.world = new TestWorld();
+    }
+
+    static function main():Void {
+        flash.Lib.current.addChild(new Main());
+    }
+}
